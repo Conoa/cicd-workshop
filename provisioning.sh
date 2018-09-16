cbb3f3903dbecdcd25473901506c3ad216c66d8c
Robert Soderlund <robban@netnerdz.se>
2018-09-16 19:14:41 +0200

    Execute provision via sudo instead of using sudo inside script

--- provisioning.sh
+++ provisioning.sh
@@ -1,10 +1,18 @@
 #!/bin/sh 
+set -x
 export DOCKERURL="https://storebits.docker.com/ee/centos/sub-7019e3a8-f1cf-434c-b454-952669b3e8b2"
-sudo -E sh -c 'echo "$DOCKERURL/centos" > /etc/yum/vars/dockerurl'
-sudo yum install -y yum-utils device-mapper-persistent-data lvm2
-sudo -E yum-config-manager --add-repo "$DOCKERURL/centos/docker-ee.repo"
-sudo yum-config-manager --enable docker-ee-stable-17.06
-sudo yum -y install docker-ee
-sudo systemctl start docker
-sudo usermod -a -G docker centos
+echo "$DOCKERURL/centos" > /etc/yum/vars/dockerurl
+yum install -y yum-utils device-mapper-persistent-data lvm2
+yum-config-manager --add-repo "$DOCKERURL/centos/docker-ee.repo"
+yum-config-manager --enable docker-ee-stable-17.06
+yum -y -q install docker-ee
+sync
+mkdir /etc/docker
+cat << EOT > /etc/docker/daemon.json
+{
+"hosts": ["unix:///var/run/docker.sock", "tcp://0.0.0.0:2375"]
+}
+EOT
+systemctl start docker
+usermod -a -G docker centos
 
